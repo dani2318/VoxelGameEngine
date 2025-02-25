20666b630f729eebe705e73c6e61afbfe881246d
dani2318 <giacche.dani@gmail.com>
2025-02-25 10:01:04 +0100

    Added basic graphics generation + initialization for shaders

--- VoxelGameEngine/VoxelGameEngine.cpp
+++ VoxelGameEngine/VoxelGameEngine.cpp
@@ -1,40 +1,64 @@
-// VoxelGameEngine.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+constexpr auto WINDOW_WIDTH = 1280;
+constexpr auto WINDOW_HEIGHT = 720;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 int main()
 {
 	if (!glfwInit()) return -1;
 
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "VoxelGameEngine", nullptr, nullptr);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);																	// Set the OpenGL version to 3.3	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);																	// 
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);													// Set the Opengl profile to core.	
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "VoxelGameEngine", nullptr, nullptr);		// Init GLFW window.
 
 	if (!window) {
-		glfwTerminate();
-		return -1;
+		glfwTerminate();																							// if the window is not initialized terminate GLFW.
+		return -1;																									// Exit the program.
 	}
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	glfwMakeContextCurrent(window);
-	gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+	glfwMakeContextCurrent(window);																					// Init OpenGL context to GLFWwindow and init glad.
+	gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);															//
+
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);																	// Set the viewport to the window size.
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 
 	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		processInput(window);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	glfwTerminate();
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
